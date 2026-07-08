a87e8be72aacec9294e534e1692a6dd33092422f
Nicolas <nicolasandreose@gmail.com>
2026-07-07 21:41:08 -0300

    feat: ensure PhotographerNotFoundException is raised

--- src/application/use-cases/get-all-photographers.ts
+++ src/application/use-cases/get-all-photographers.ts
@@ -1,10 +1,15 @@
 import { PhotographerEntity } from "../../domain/entities/photographer";
 import { IPhotographerRepository } from "../../domain/ports/photographer";
+import { PhotographerNotFoundException } from "../../exceptions/photographer";
 
 export class GetAllPhotographersUseCase {
   constructor(private repository: IPhotographerRepository) {}
 
   async execute(): Promise<PhotographerEntity[]> {
-    return this.repository.getAll();
+    const photographers = await this.repository.getAll();
+    if (photographers.length === 0) {
+      throw new PhotographerNotFoundException();
+    }
+    return photographers;
   }
 }
