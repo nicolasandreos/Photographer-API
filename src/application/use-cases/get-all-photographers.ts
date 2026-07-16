c24ef342039b2ee1aabbfe1a459d5d1650d36fca
Nicolas <nicolasandreose@gmail.com>
2026-07-16 20:28:41 -0300

    fix: ensure get all photograpger route return 200 when not found

--- src/application/use-cases/get-all-photographers.ts
+++ src/application/use-cases/get-all-photographers.ts
@@ -1,15 +1,11 @@
 import { PhotographerEntity } from "../../domain/entities/photographer";
 import { IPhotographerRepository } from "../../domain/repositories/photographer";
-import { PhotographerNotFoundException } from "../../exceptions/photographer";
 
 export class GetAllPhotographersUseCase {
   constructor(private repository: IPhotographerRepository) {}
 
   async execute(): Promise<PhotographerEntity[]> {
     const photographers = await this.repository.getAll();
-    if (photographers.length === 0) {
-      throw new PhotographerNotFoundException();
-    }
     return photographers;
   }
 }
